1c3badcd28128d943d56664953c03a0664d47c9a
KatVHarris <kvm.harris@gmail.com>
2014-09-07 02:09:08 -0700

    get data

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -2,6 +2,10 @@ $(document).ready(function(){
 	var goallist = [];
 	var subcriberlist = [];
 
+	$.get('/api/campaign', function(data){
+		console.log(data);
+	})
+
 	$('.input-list li').click(function(evt){
 		//add item to goallist
 		// add a new item to the middle

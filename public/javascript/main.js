ec0852575f8083e503a2ef08380b7f691dee92d4
bitchwhocodes <stacey.mulcahy@gmail.com>
2014-09-07 03:06:43 -0400

    added in list

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
 	
+
+
+
 	$('.input-list li').click(function(evt){
 		// add a new item to the middle
 		console.log("these have been fired");
 		evt.stopPropagation();
+
 
 	})
 
@@ -17,5 +21,15 @@ $(document).ready(function(){
 
 		var val = $('#CampaignDescription').val();
 		console.log(val);
+	})
+
+	$('.results-list li').click(function(evt){
+		if($(this).hasClass('selected'))
+		{
+			$(this).removeClass('selected')
+		}else{
+
+			$(this).addClass('selected')
+		}
 	})
 })

cc52bbc6462d48a74249c63d4e6c930e118f7d9e
KatVHarris <kvm.harris@gmail.com>
2014-09-06 22:12:48 -0700

    Kat js linking

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -6,4 +6,18 @@ $(document).ready(function(){
 		evt.stopPropagation();
 
 	})
+
+	$('#submit-campaign').click(function(evt){
+		//add a new Campaign...Need to connect this to the backend somehow...
+//		$.post("/", function( data ) {
+
+		console.log($('#campaigndescription').val());
+//	    })
+//		console.log("submit clicked");
+//		evt.stopPropagation();
+	})
+
+	$.get("/api/campaign", function( data ) {
+	  console.log(data);
+	});
 })
